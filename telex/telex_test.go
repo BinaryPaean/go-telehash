fc617ed10ba172365ece193934a00bff2bc34db8
Aaron Stewart <aaron@binarypaean.com>
2012-08-26 10:28:01 -0700

    Pulled in gocheck.

--- telex/telex_test.go
+++ telex/telex_test.go
@@ -1,17 +1,24 @@
 package telex
 
 import (
+	"launchpad.net/gocheck"
 	"testing"
 )
 
-func TestTelexFromJson(t *testing.T) {
+func Test(t *testing.T) { gocheck.TestingT(t) }
+
+type TelexSuite struct{}
+
+var _ = gocheck.Suite(&TelexSuite{})
+
+func (s *TelexSuite) TestTelexFromJson(c *gocheck.C) {
 	const test_json = `{
       "_ring":43723,
       ".see":["5.6.7.8:23456","11.22.33.44:11223"],
     }`
 	tx, err := TelexFromJson(test_json)
-	if err != nil {
-		t.Errorf("Parsing telex json failed!")
-	}
-	t.Log(tx)
+	c.Check(err, gocheck.IsNil)
+	h1 := &map[string]string{"_ring": "43723"}
+	var headers = [](*map[string]string){h1}
+	c.Check(tx.headers, gocheck.DeepEquals, headers)
 }
