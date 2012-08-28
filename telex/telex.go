be0d075346a4924270c0c2c9c2139eb9bb8a0e45
Aaron Stewart <aaron@binarypaean.com>
2012-08-28 11:42:44 -0700

    Basic unmarshaling test passing.

--- telex/telex.go
+++ telex/telex.go
@@ -1,20 +1,65 @@
 package telex
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 )
 
-type UA []interface{} //Untyped Array
+type JsonObject map[string]interface{}
+
 type Telex struct {
-	Headers  UA
-	Commands UA
-	Signals  UA
-	Body     UA
-	Raw      UA
+	Headers  *Headers
+	Commands *Commands
+	Signals  JsonObject
+	Raw      JsonObject
+	Errors   []error
 }
 
-func TelexFromJson(json_in []byte) (*Telex, error) {
-	t := new(Telex)
-	err := json.Unmarshal(json_in, t.Raw)
-	return t, err
+type Headers struct {
+	To   string `json:"_to"`
+	Ring int64  `json:"_ring"`
+	Line int64  `json:"_line"`
+	Br   int64  `json:"_br"`
+	Hop  int    `json:"_hop"`
+}
+
+type Commands struct {
+	See []string `json:".see"`
+	Tap []string `json:".tap`
+}
+
+func TelexFromJson(json_in []byte) (t *Telex) {
+	t = new(Telex)
+	t.unmarshalInto(json_in, &t.Raw)
+	t.unmarshalInto(json_in, &t.Commands)
+	t.unmarshalInto(json_in, &t.Headers)
+	return t
+}
+
+func (t *Telex) unmarshalInto(json_in []byte, target interface{}) {
+	err := json.Unmarshal(json_in, &target) //Load the entire JSON object into Raw
+	if err != nil {
+		t.Errors = append(t.Errors, err)
+	}
+}
+
+func (t *Telex) String() string {
+	b := new(bytes.Buffer)
+	for k, v := range t.Raw {
+		switch vv := v.(type) {
+		case string:
+			b.WriteString(fmt.Sprintln(k, "(string):", vv))
+		case float64:
+			b.WriteString(fmt.Sprintln(k, "(float64):", vv))
+		case []interface{}:
+			b.WriteString(fmt.Sprintln(k, "(array):"))
+			for i, u := range vv {
+				b.WriteString(fmt.Sprintln(i, u))
+			}
+		default:
+			b.WriteString(fmt.Sprintln(k, "is of type: ", vv))
+		}
+	}
+	return b.String()
 }
--- telex/telex_test.go
+++ telex/telex_test.go
@@ -6,6 +6,36 @@ import (
 	"testing"
 )
 
+const (
+	simple_telex_json = `{
+      "_ring":43723,
+      ".see":["5.6.7.8:23456","11.22.33.44:11223"]
+    }`
+
+	command_telex_json = `{
+      "+end":"a9993e364706816aba3e25717850c26c9cd0d89d",
+      "+foo":"0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33"
+    }`
+
+	normal_telex_json = `{
+      "_to":"1.2.3.4:5678",
+      "_line":63546230,
+      "profile_image_url": "http://a3.twimg.com/profile_images/852841481/Untitled_3_normal.jpg",
+      "created_at": "Sat, 08 May 2010 21:46:23 +0000",
+      "from_user": "pelchiie",
+      "metadata": {
+        "result_type": "recent"
+      },
+      "to_user_id": null,
+      "text": "twitter is dead today.",
+      "id": 13630378882,
+      "from_user_id": 12621761,
+      "geo": null,
+      "iso_language_code": "en",
+      "source": "<a href="http://twitter.com/">web</a>"
+    }`
+)
+
 func Test(t *testing.T) { gocheck.TestingT(t) }
 
 type TelexSuite struct{}
@@ -13,17 +43,11 @@ type TelexSuite struct{}
 var _ = gocheck.Suite(&TelexSuite{})
 
 func (s *TelexSuite) TestTelexFromJson(c *gocheck.C) {
-	const test_json = `{
-      "_ring":43723,
-      ".see":["5.6.7.8:23456","11.22.33.44:11223"],
-    }`
-	tx, err := TelexFromJson([]byte(test_json))
-	c.Check(err, gocheck.IsNil)
-	headers := []interface{}{map[interface{}]interface{}{"_ring": 43723}}
-	commands := []interface{}{map[interface{}]interface{}{".see": []interface{}{"5.6.7.8:23456", "11.22.33.44:11223"}}}
-	c.Check(tx.Headers, gocheck.DeepEquals, headers)
-	c.Check(tx.Commands, gocheck.DeepEquals, commands)
-	var blank = new([]interface{})
-	err = json.Unmarshal([]byte(test_json), &blank)
-	c.Check(tx.Raw, gocheck.Equals, blank)
+	tx := TelexFromJson([]byte(simple_telex_json))
+	c.Check(len(tx.Errors), gocheck.Equals, 0)
+	c.Check(tx.Headers.Ring, gocheck.DeepEquals, int64(43723))
+	c.Check(tx.Commands.See, gocheck.DeepEquals, []string{"5.6.7.8:23456", "11.22.33.44:11223"})
+	blank := new(JsonObject)
+	json.Unmarshal([]byte(simple_telex_json), &blank)
+	c.Check(tx.Raw, gocheck.DeepEquals, *blank)
 }
