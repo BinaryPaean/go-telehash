5e0f47abc19a7a883088a1327d8919f2e305771f
Aaron Stewart <aaron@binarypaean.com>
2012-08-31 18:39:17 -0700

    Refactored telex to contain category fields rather than individual fields. For example, Telex.Body or Telex.Headers are now each map[string]interface{} fields. Telex.Ring, Telex.See, etc fields have been removed.

--- telex/telex.go
+++ telex/telex.go
@@ -5,64 +5,54 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"regexp"
 )
 
 //convenience type for dealing with encoding/json's results.
 type jsonObject map[string]interface{}
 
-//Telex is the primary object of interest in the package, and includes a reference to its set of headers and commands.
-type Telex struct {
-	Headers  *Headers
-	Commands *Commands
-	Signals  jsonObject
-	Raw      jsonObject
-	Errors   []error
-}
-
+//Telex is the primary object of interest in the package.
 //Headers are optionally included in each telex message.
 //   To - A string value of the public IP:PORT that the Telex was sent to (helps recipient with NATs)
 //   Ring - Used to open a Line with a Switch, integer value from 1 to 32768
 //   Line - The private unique id of the line from one Switch to another, the product of the _ring from both
 //   Br - Bytes Received, always tell the recipient the total bytes that have been received from them so far
 //   Hop - Integer value from 0 to 4, incremented any time the Telex is forwarded
-type Headers struct {
-	To   string `json:"_to"`
-	Ring int64  `json:"_ring"`
-	Line int64  `json:"_line"`
-	Br   int64  `json:"_br"`
-	Hop  int    `json:"_hop"`
-}
-
 //Commands are optionally included in each telex message. 
 //    See - An array of other Switches (IP:PORT) that the recipient may find useful.
 //    Tap - An array of filters that describe which telexes to match and forward back to the requesting Switch.
-type Commands struct {
-	See []string `json:".see"`
-	Tap []string `json:".tap`
+type Telex struct {
+	Commands jsonObject `json:"-"`
+	Headers  jsonObject `json:"-"`
+	Signals  jsonObject `json:"-"`
+	Body     jsonObject `json:"-"`
+}
+
+//NewTelex returns an initialized Telex struct, ready for use.
+func NewTelex() *Telex {
+	t := new(Telex)
+	t.Body = make(jsonObject)
+	t.Signals = make(jsonObject)
+	t.Headers = make(jsonObject)
+	t.Commands = make(jsonObject)
+	return t
 }
 
 //TelexFromJson attempts to decode the provided json in json_in into a new telex. Note that
 //TelexFromJson does not return errors, and always returns a new Telex. In the event of parsing errors
-//some or all of the telex's fields may be blank, and errors will be reported in t.Errors []error.
-func TelexFromJson(json_in []byte) (t *Telex) {
-	t = new(Telex)
-	t.unmarshalInto(json_in, &t.Raw)
-	t.unmarshalInto(json_in, &t.Commands)
-	t.unmarshalInto(json_in, &t.Headers)
-	return t
+//some or all of the telex's fields may be blank, and errors will be reported in the t.Errors field, as a slice of errors.
+func TelexFromJson(json_in []byte) (*Telex, error) {
+	t := NewTelex()
+	j := new(jsonObject)
+	err := json.Unmarshal(json_in, j)
+	t.extractFieldsFrom(*j)
+	return t, err
 }
 
-func (t *Telex) unmarshalInto(json_in []byte, target interface{}) {
-	err := json.Unmarshal(json_in, &target) //Load the entire JSON object into Raw
-	if err != nil {
-		t.Errors = append(t.Errors, err)
-	}
-}
-
-//String formats a telex as a semi human-readable string. See ToJson to serialize a telex.
+//String formats a telex as a semi human-readable string. Use ToJson to serialize a telex instead.
 func (t *Telex) String() string {
 	b := new(bytes.Buffer)
-	for k, v := range t.Raw {
+	for k, v := range t.Signals {
 		switch vv := v.(type) {
 		case string:
 			b.WriteString(fmt.Sprintln(k, "(string):", vv))
@@ -80,6 +70,26 @@ func (t *Telex) String() string {
 	return b.String()
 }
 
-func (t *Telex) ToJson() []bytes {
-	return nil
+func (t *Telex) extractFieldsFrom(j jsonObject) {
+	signal_matcher := regexp.MustCompile(`^\+(.)+`)
+	command_matcher := regexp.MustCompile(`^\.(.)+`)
+	header_matcher := regexp.MustCompile(`^\_(.)+`)
+	for k, v := range j {
+		target := []byte(k)
+		switch {
+		default:
+			t.Body[k] = v
+		case signal_matcher.Match(target):
+			t.Signals[k] = v
+		case command_matcher.Match(target):
+			t.Commands[k] = v
+		case header_matcher.Match(target):
+			t.Headers[k] = v
+		}
+	}
+}
+
+func (t *Telex) ToJson() []byte {
+	buf := new(bytes.Buffer)
+	return buf.Bytes()
 }
--- telex/telex_test.go
+++ telex/telex_test.go
@@ -1,10 +1,7 @@
 package telex
 
-import (
-	"encoding/json"
-	"launchpad.net/gocheck"
-	"testing"
-)
+import . "launchpad.net/gocheck"
+import "testing"
 
 const (
 	simple_telex_json = `{
@@ -18,8 +15,8 @@ const (
     }`
 
 	normal_telex_json = `{
-      "_to":"1.2.3.4:5678",
-      "_line":63546230,
+      "_to": "1.2.3.4:5678",
+      "_line": 63546230,
       "profile_image_url": "http://a3.twimg.com/profile_images/852841481/Untitled_3_normal.jpg",
       "created_at": "Sat, 08 May 2010 21:46:23 +0000",
       "from_user": "pelchiie",
@@ -32,22 +29,32 @@ const (
       "from_user_id": 12621761,
       "geo": null,
       "iso_language_code": "en",
-      "source": "<a href="http://twitter.com/">web</a>"
+      "source": "<a href=\"http://twitter.com/\">web</a>"
     }`
 )
 
-func Test(t *testing.T) { gocheck.TestingT(t) }
+func Test(t *testing.T) { TestingT(t) }
 
 type TelexSuite struct{}
 
-var _ = gocheck.Suite(&TelexSuite{})
+var _ = Suite(&TelexSuite{})
 
-func (s *TelexSuite) TestTelexFromJson(c *gocheck.C) {
-	tx := TelexFromJson([]byte(simple_telex_json))
-	c.Check(len(tx.Errors), gocheck.Equals, 0)
-	c.Check(tx.Headers.Ring, gocheck.DeepEquals, int64(43723))
-	c.Check(tx.Commands.See, gocheck.DeepEquals, []string{"5.6.7.8:23456", "11.22.33.44:11223"})
-	blank := new(jsonObject)
-	json.Unmarshal([]byte(simple_telex_json), &blank)
-	c.Check(tx.Raw, gocheck.DeepEquals, *blank)
+func (s *TelexSuite) TestSimpleTelexFromJson(c *C) {
+	tx, err := TelexFromJson([]byte(simple_telex_json))
+	c.Check(err, Equals, nil)
+	c.Check(tx.Headers["_ring"], DeepEquals, float64(43723))
+	c.Check(tx.Commands[".see"], DeepEquals, []interface{}{"5.6.7.8:23456", "11.22.33.44:11223"})
+
+}
+
+func (s *TelexSuite) TestSignalsTelexFromJson(c *C) {
+	tx, err := TelexFromJson([]byte(signals_telex_json))
+	c.Check(err, Equals, nil)
+	c.Check(tx.Signals["+end"], Equals, "a9993e364706816aba3e25717850c26c9cd0d89d")
+	c.Check(tx.Signals["+foo"], Equals, "0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33")
+}
+
+func (s *TelexSuite) TestNormalTelexFromJson(c *C) {
+	_, err := TelexFromJson([]byte(normal_telex_json))
+	c.Check(err, Equals, nil)
 }
